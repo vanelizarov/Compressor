71e0e48bef45b1ac75b9aab85f89092b5291dda3
vanelizarov <elizarov.vanya@gmail.com>
2017-03-06 02:25:46 +0300

    Refactored main container. Added FrameViews for all transformation types

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -1,14 +1,13 @@
 import React, {Component} from 'react';
 
-// import {bindActionCreators} from 'redux';
-// import {connect} from 'react-redux';
-
 import FrameView from './frameView';
 import VirtualVideo from './virtualVideo';
 import Logo from './logo';
 
 import huffman from '../logic/huffman';
-
+import rle from '../logic/rle';
+import lzw from '../logic/lzw';
+import curved from '../logic/curved';
 
 import {
     Grid,
@@ -28,7 +27,8 @@ class App extends Component {
 
         return (
             <div>
-                <Navbar inverse={true} fluid={true}>
+                <Navbar inverse={true}
+                        fluid={true}>
                     <Navbar.Header>
 
                         <Navbar.Brand>
@@ -45,19 +45,73 @@ class App extends Component {
                     </Navbar.Collapse>
                 </Navbar>
                 <VirtualVideo />
-                <Grid>
+                <Grid fluid={true}>
                     <Row>
-                        <Col xs={12} sm={6}>
-                            <FrameView original={true}/>
-                            <FrameView original={false}/>
-                            <FrameView original={false}/>
+                        <Col xs={12} sm={4}>
+                            <FrameView original={true}
+                                       title={'Original'}/>
                         </Col>
-                        <Col xs={12} sm={6}>
-                            <FrameView original={false} encodeFunc={huffman.encode} decodeFunc={huffman.decode} compType={types.HUFFMAN_COMP} uncompType={types.HUFFMAN_UNCOMP}/>
-                            <FrameView original={false}/>
-                            <FrameView original={false}/>
+                        <Col xs={12} sm={4}>
+                            <FrameView original={false}
+                                       title={'Huffman encoded'}
+                                       reformatFunc={huffman.adapt}
+                                       compType={types.HUFFMAN_COMP}/>
+                        </Col>
+                        <Col xs={12} sm={4}>
+                            <FrameView original={false}
+                                       title={'Huffman decoded'}
+                                       encodeFunc={huffman.encode}
+                                       decodeFunc={huffman.decode}
+                                       compType={types.HUFFMAN_COMP}
+                                       uncompType={types.HUFFMAN_UNCOMP}/>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col xs={12} sm={4}>
+                            <FrameView original={false}
+                                       title={'Logarithmic'}
+                                       encodeFunc={curved.log}
+                                       compType={types.LOG_COMP}/>
+                        </Col>
+                        <Col xs={12} sm={4}>
+                            <FrameView original={false}
+                                       title={'RLE encoded'}
+                                       reformatFunc={rle.adapt}
+                                       compType={types.RLE_COMP}/>
+                        </Col>
+                        <Col xs={12} sm={4}>
+                            <FrameView original={false}
+                                       title={'RLE decoded'}
+                                       encodeFunc={rle.encode}
+                                       decodeFunc={rle.decode}
+                                       compType={types.RLE_COMP}
+                                       uncompType={types.RLE_UNCOMP}/>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col xs={12} sm={4}>
+
+                            <FrameView original={false}
+                                       title={'Exponential'}
+                                       encodeFunc={curved.exp}
+                                       compType={types.EXP_COMP}/>
+
                         </Col>
 
+                        <Col xs={12} sm={4}>
+                            <FrameView original={false}
+                                       title={'LZW encoded'}
+                                       reformatFunc={lzw.adapt}
+                                       compType={types.LZW_COMP}/>
+                        </Col>
+                        <Col xs={12} sm={4}>
+                            <FrameView original={false}
+                                       title={'LZW decoded'}
+                                       encodeFunc={lzw.encode}
+                                       decodeFunc={lzw.decode}
+                                       compType={types.LZW_COMP}
+                                       uncompType={types.LZW_UNCOMP}/>
+                        </Col>
                     </Row>
                 </Grid>
             </div>
